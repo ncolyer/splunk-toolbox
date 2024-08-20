1eadf69426a450e234aacb4b13bade74eac17e36
Nicholas Colyer <ncolyer@gmail.com>
2024-08-20 08:48:37 -0400

    feat: Update generatemarkdown.sh script to improve URL generation and add verb documentation

--- .utilities/generatemarkdown.sh
+++ .utilities/generatemarkdown.sh
@@ -3,33 +3,49 @@
 # Function to create markdown file with Mermaid diagram
 create_markdown() {
     local directory="$1"
-    local foldername
-    local foldername_safe
-    local markdown_file
     local subdir_basename
     local subdir_safe
     local subdirs=()
     local letter="B"
 
+    # Repository URL
+    local github_url
+    github_url="https://github.com/ncolyer/splunk-toolbox/blob/main/data"
+    
+    # Folder name
+    local foldername
     foldername=$(basename "$directory")
-    markdown_file="$directory/${foldername}.md"
+    
+    # Markdown file name
+    local markdown_file
+    markdown_file="$directory/README.md"
 
     # Replace spaces and hyphens with underscores for Mermaid compatibility
+    local foldername_safe
     foldername_safe=$(echo "$foldername" | sed 's/ /_/g' | sed 's/-/_/g')
 
     # Find subdirectories
+    #-----------------------------------------------------------------------
+    # - mindepth 1: exclude the directory itself
+    # - maxdepth 1: only include the immediate children
+    # - type d: only include directories
     while IFS= read -r -d '' subdir; do
         subdirs+=("$subdir")
     done < <(find "$directory" -mindepth 1 -maxdepth 1 -type d -print0)
 
+
+    # Create the Mermaid diagram if there are subdirectories
+    #-----------------------------------------------------------------------
+    # - If there are subdirectories, create the Mermaid diagram
+    # - If there are no subdirectories, do not create the Mermaid diagram
     if [ ${#subdirs[@]} -gt 0 ]; then
-        # Create the Mermaid diagram structure
         {
+            # Create the Mermaid diagram structure
             echo "# $foldername"
             echo
             echo '```mermaid'
             echo "graph TD;"
-            echo "    Z[<a href=\"../\" target=\"_blank\" style=\"color:black\">Go Back to Index</a>]:::backLevelStyle"
+            echo "    Z[<a href=\"$github_url\" target=\"_blank\" style=\"color:black\">Go Back</a>]:::backLevelStyle"
             echo "    Z --> A[$foldername_safe]:::firstLevelStyle"
 
             for subdir in "${subdirs[@]}"; do
@@ -38,7 +54,7 @@ create_markdown() {
 
                 # Generate the correct URL by replacing the path
                 if [ -n "$(find "$subdir" -mindepth 1 -print -quit)" ]; then
-                    url_path="https://github.com/ncolyer/splunk-toolbox/blob/main/data/${subdir_safe}.md"
+                    url_path="${github_url}/${subdir_safe}/"
                     # Use secondLevelStyle for non-empty subdirectories with correct hyperlink
                     echo "    A --> ${letter}[<a href=\"$url_path\" target=\"_blank\" style=\"color:white\">$subdir_basename</a>]:::secondLevelStyle"
                 else
@@ -56,13 +72,41 @@ create_markdown() {
             echo "    classDef secondLevelStyle fill:#34495e,stroke:#333,stroke-width:2px;"
             echo "    classDef secondLevelStyleMissing fill:#000000,stroke:#333,stroke-width:2px;"
             echo '```'
-        } > "$markdown_file"
+        } >"$markdown_file"
     fi
+}
+
+create_verb_markdown() {
+    local directory="$1"
+    local markdown_file="$directory/README.md"
+
+    # Print the header
+    {
+        echo " "
+        echo " # $foldername Verb Documentation"
+        echo " "
+    } >> "$markdown_file"
+    
+    printf "%-25s %-25s %-25s %-25s\n" "Category" "Type" "Functionality" "Specifics" >> "$markdown_file"
+
+    # Find all .spl files in the specified directory, process and format their names
+    find "$directory" -type f -name "*.spl" | while read -r file; do
+
+        # Extract the filename without the path and extension
+        filename=$(basename "$file" .spl)
+        
+        # Split the filename into an array using underscore as a delimiter
+        IFS='_' read -r -a parts <<< "$filename"
+        
+        # Print the first four elements of the array, formatted for even spacing, and append to the markdown file
+        printf "%-25s %-25s %-25s %-25s\n" "${parts[0]}" "${parts[1]}" "${parts[2]}" "${parts[3]}" >> "$markdown_file"
+    done
 }
 
 # Traverse directories and create markdown files
 find . -type d -not -path '*/\.*' -not -path './data*' | while read -r directory; do
     if [ "$directory" != "." ]; then
         create_markdown "$directory"
+        create_verb_markdown "$directory"
     fi
 done
---
+++ tools/README.md
@@ -0,0 +1,18 @@
+# tools
+
+```mermaid
+graph TD;
+    Z[<a href="https://github.com/ncolyer/splunk-toolbox/blob/main/data" target="_blank" style="color:black">Go Back</a>]:::backLevelStyle
+    Z --> A[tools]:::firstLevelStyle
+    A --> B[<a href="https://github.com/ncolyer/splunk-toolbox/blob/main/data/speculative_data_modeling/" target="_blank" style="color:white">speculative_data_modeling</a>]:::secondLevelStyle
+
+    classDef backLevelStyle fill:#e74c3c,stroke:#333,stroke-width:2px,color:#ffffff;
+    classDef firstLevelStyle fill:#6188b9,stroke:#333,stroke-width:2px;
+    classDef secondLevelStyle fill:#34495e,stroke:#333,stroke-width:2px;
+    classDef secondLevelStyleMissing fill:#000000,stroke:#333,stroke-width:2px;
+```
+ 
+ #  Verb Documentation
+ 
+Category                  Type                      Functionality             Specifics                
+utility                   dimension                 enumeration               all                      
